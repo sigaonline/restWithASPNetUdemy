c376766e3623d7bebdddec47160363bd249d7312
Alex Freitas <gynarf@hotmail.com>
2020-08-19 15:46:23 -0300

    Impementando Calculadora Rest

---
+++ restWithASPNetUdemy/restWithASPNetUdemy 01 - Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace restWithASPNetUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalculatorController : Controller
+    {
+
+
+        // DELETE api/values/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(number, out decimalValue))
+                return decimalValue;
+
+            return 0;
+
+        }
+
+        private bool IsNumeric(string number)
+        {
+            double numberDouble;
+
+            bool isNumber = double.TryParse(number, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out numberDouble);
+
+            return isNumber;
+
+        }
+    }
+}
